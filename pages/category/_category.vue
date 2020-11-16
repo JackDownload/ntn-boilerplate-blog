8e95eb5ad25364475969aeaefdedf8a5de86c2d4
Mocsonoky Attila <kismocsy@gmail.com>
2020-11-16 12:00:40 +0100

    dsf

---
+++ pages/category/_category.vue
@@ -0,0 +1,17 @@
+<template>
+  <article v-if="categoryPost" class="main article">
+    <h1 class="article-title">{{ categoryPost.title }}</h1>
+    <p class="mt-4">{{ categoryPost.description }}</p>
+  </article>
+</template>
+<script>
+export default {
+  async asyncData({ params, payload }) {
+    if (payload) return { categoryPost: payload }
+    else
+      return {
+        categoryPost: await require(`~/assets/content/categorys/${params.category}.json`),
+      }
+  },
+}
+</script>
