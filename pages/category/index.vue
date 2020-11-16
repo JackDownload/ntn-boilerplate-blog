aa038ec9811b265a21352e33b7b6c94f1421f6cc
Mocsonoky Attila <kismocsy@gmail.com>
2020-11-16 11:47:32 +0100

    das

--- pages/category/index.vue
+++ pages/category/index.vue
@@ -1,9 +1,9 @@
 <template>
-  <main v-if="categoryPost" class="main">
-    <h1 class="title text-left">CategoryPost</h1>
-    <ul v-for="(categoryPost, index) in categoryPost" :key="index" class="articles">
-      <nuxt-link :to="`category/${categoryPost.slug}`" class="article article--clickable">
-        <div class="flex justify-between align-baseline"></div>
+  <main v-if="categoryPosts" class="main">
+    <h1 class="title text-left">Projects</h1>
+    <ul v-for="(categoryPost, index) in categoryPosts" :key="index" class="articles">
+      <nuxt-link class="article article--clickable" :to="`category/${categoryPost.slug}`">
+        <h3 class="article-title">{{ categoryPost.title }}</h3>
         <div class="mt-4 mb-2">
           <p class="inline">{{ categoryPost.description }}</p>
         </div>
@@ -16,12 +16,6 @@ export default {
   computed: {
     categoryPosts() {
       return this.$store.state.categoryPosts
-    },
-  },
-  methods: {
-    formatDate(dateString) {
-      const date = new Date(dateString)
-      return date.toLocaleDateString(process.env.lang) || ''
     },
   },
 }
